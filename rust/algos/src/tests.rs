13578fd436955e7eeb2b2864b23155e7d73cdf37
Alexander Heim <alexanderheimat@gmail.de>
2020-09-22 11:40:43 +0200

    added rust implementation

---
+++ rust/algos/src/tests.rs
@@ -0,0 +1,50 @@
+use super::*;
+
+#[test]
+fn sequencial_search_test_1(){
+    let vector = vec![1, 2, 3, 4, 5];
+    let result = sequencial_search(&vector, 3);
+    assert_eq!(result.unwrap(), 2);
+}
+
+#[test]
+fn sequencial_search_test_2(){
+    let vector = vec![1, 2, 4, 5];
+    let result = sequencial_search(&vector, 3);
+    assert!(result.is_none());
+}
+
+#[test]
+fn sequencial_search_test_3(){
+    let vector = vec![5, 2, 2, 3];
+    let result = sequencial_search(&vector, 3);
+    assert!(result.is_some());
+}
+
+#[test]
+fn binary_search_test_1(){
+    let vector = vec![0, 2, 4, 6, 8];
+    let result = binary_search(&vector, 6);
+    assert_eq!(result.unwrap(), 3);
+}
+
+#[test]
+fn binary_search_test_2(){
+    let vector = vec![0, 2, 4, 5, 6, 7, 12, 33, 412, 3000];
+    let result = binary_search(&vector, 0);
+    assert_eq!(result.unwrap(), 0);
+}
+
+#[test]
+fn binary_search_test_3(){
+    let vector = vec![1, 2, 4];
+    let result = binary_search(&vector, 3);
+    assert!(result.is_none());
+}
+
+#[test]
+fn binary_search_test_4(){
+    let vector = vec![1];
+    let result = binary_search(&vector, 2);
+    assert!(result.is_none());
+}
